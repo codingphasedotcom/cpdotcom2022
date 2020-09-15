84d780494b5254c55fb9793ea5e9dfe8fa00c5fe
joe santos garcia <joesantosgarcia@gmail.com>
2020-09-14 23:50:46 -0400

    lazyload

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../assets/scss/main.scss'
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
