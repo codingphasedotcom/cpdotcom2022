d0969312831e3ad2965b422f196164326ae36207
joe santos garcia <joesantosgarcia@gmail.com>
2020-10-14 22:36:29 -0400

    added gtag v4

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,6 @@ class MyDocument extends Document {
         
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous"></link>
-          <!-- Global site tag (gtag.js) - Google Analytics -->
               <script async src="https://www.googletagmanager.com/gtag/js?id=G-E6VQ9KSVFF"></script>
           <script
             dangerouslySetInnerHTML={{
