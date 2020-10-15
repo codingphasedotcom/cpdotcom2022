ffa90f1aebec8ab9e07681ce06205d319671a312
joe santos garcia <joesantosgarcia@gmail.com>
2020-10-14 22:34:25 -0400

    added gtag v4

--- pages/_document.js
+++ pages/_document.js
@@ -13,19 +13,19 @@ class MyDocument extends Document {
         
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous"></link>
-          {/* <!-- Global site tag (gtag.js) - Google Analytics -->
-              <script async src="https://www.googletagmanager.com/gtag/js?id=UA-37043736-16"></script>
+          <!-- Global site tag (gtag.js) - Google Analytics -->
+              <script async src="https://www.googletagmanager.com/gtag/js?id=G-E6VQ9KSVFF"></script>
           <script
             dangerouslySetInnerHTML={{
               __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-              
-                gtag('config', 'UA-37043736-16');
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'G-E6VQ9KSVFF');
               `
             }}
-          /> */}
+          />
         </Head>
         <body>
           <Main />
