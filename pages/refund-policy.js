d706d205c0c984f3fc418b93cb6a7cc71032b27e
joe santos garcia <joesantosgarcia@gmail.com>
2020-10-16 00:31:55 -0400

    fixed refund policy

--- pages/refund-policy.js
+++ pages/refund-policy.js
@@ -34,13 +34,12 @@ export default function Home() {
       <MainLayout>
         <section className="page">
             <div className="container">
-                <h1>Refund Policy</h1>
 
 <h1>Refund Policy</h1>
 <h2>Refund policy on Course Purchases</h2>
 <p>We’re so convinced you’ll absolutely love our services, that we’re willing to offer a 7 day risk-free money back guarantee. If you are not satisfied with the service for any reason you can get a refund within 7 days of making a purchase.</p>
 <h2>Refund policy on Subscription</h2>
-<p>When it comes to subscriptions either monthly or yearly that we’re willing to offer a 7 day risk-free money back guarantee on your first payment. Anything beyond the first payment is no longer covered to a refund. Remember this is a subscription you will get charged on a monthly basis or a yearly basis. It’s the customers responsibility to cancel their membership before getting charged again you can either contact us at <strong>codingphase@gmail.com</strong> and we will cancel your subscription or go through your account settings and cancel your subscription manually.</p>
+<p>When it comes to subscriptions either monthly or yearly we’re willing to offer a 7 day risk-free money back guarantee on your first payment. Anything beyond the first payment is no longer covered to a refund. Remember this is a subscription you will get charged on a monthly basis or a yearly basis. It’s the customers responsibility to cancel their membership before getting charged again you can either contact us at <strong>codingphase@gmail.com</strong> to cancel your subscription or go through your account settings and cancel your subscription manually.</p>
 
             </div>
         </section>
