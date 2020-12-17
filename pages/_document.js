ff60d34056a89cddf9b8044da9279e57c43bcd63
joe santos garcia <joesantosgarcia@gmail.com>
2020-12-17 14:35:10 -0500

    Add google tag manager

--- pages/_document.js
+++ pages/_document.js
@@ -25,6 +25,17 @@ class MyDocument extends Document {
               `
             }}
           />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-58ZXPD5');
+              `
+            }}
+          />
 
 <script
             dangerouslySetInnerHTML={{
@@ -53,6 +64,14 @@ class MyDocument extends Document {
           />
         </Head>
         <body>
+        <noscript
+            dangerouslySetInnerHTML={{
+              __html: `
+              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-58ZXPD5"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>
+              `
+            }}
+          />
           <Main />
           <NextScript />
         </body>
