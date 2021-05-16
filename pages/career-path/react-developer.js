6db3ce56ac1e191fcd351e1551c491ff8f9d53c4
codingphase <codingphase@gmail.com>
2021-05-15 20:29:18 -0400

    added react streaming service

--- pages/career-path/react-developer.js
+++ pages/career-path/react-developer.js
@@ -279,7 +279,20 @@ export default function Home() {
                       </div>
                     </div>
                   </div>
-                  
+                  <div className="course">
+                    <div className="row g-3">
+                      <div className="col-md-1">
+                        <img src="https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=width:705/https://www.filepicker.io/api/file/9Wf8JuAQCY6sp17W0Ggm" className="course-img" />
+                      </div>
+                      <div className="col-md-10">
+                        <Link href="https://codingphase.teachable.com/courses/enrolled/1410358">
+                          <a>
+                            <span className="title">React JS - Build A Streaming Service</span>
+                          </a>
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
                   <div className="course">
                     <div className="row g-3">
                       <div className="col-md-1">
