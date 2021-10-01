3ab25f311325ac254b79d625073c20b60a482d42
codingphase <codingphase@gmail.com>
2021-10-01 13:33:05 -0400

    no message

--- pages/_document.js
+++ pages/_document.js
@@ -100,13 +100,7 @@ drift.load('7xrggzxt4wz5');
               `
             }}
           /> */}
-          <script type="application/javascript"
-            dangerouslySetInnerHTML={{
-              __html: `
-              (function(b,o,n,g,s,r,c){if(b[s])return;b[s]={};b[s].scriptToken="Xy00NTY0ODQyNzU";b[s].callsQueue=[];b[s].api=function(){b[s].callsQueue.push(arguments);};r=o.createElement(n);c=o.getElementsByTagName(n)[0];r.async=1;r.src=g;r.id=s+n;c.parentNode.insertBefore(r,c);})(window,document,"script","https://cdn.oribi.io/Xy00NTY0ODQyNzU/oribi.js","ORIBI");
-              `
-            }}
-          />
+
 
 <script type="application/javascript"
             dangerouslySetInnerHTML={{
