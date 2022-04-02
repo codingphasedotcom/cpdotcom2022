aa1cd59b1381de3e2cdb403e088cdc0a61f79417
CodingPhase <joesantosgarcia@gmail.com>
2022-04-02 00:28:58 -0400

    added shopify blog

---
+++ pages/career/how-to-become-a-shopify-developer-ultimate-roadmap.js
@@ -0,0 +1,181 @@
+import React, { useState, useEffect } from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+import MainLayout from '../../components/layouts/MainLayout';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import ReactGA from 'react-ga';
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+
+export default function Home() {
+    useEffect(() => {
+        ReactGA.initialize('UA-37043736-10');
+        if (typeof window !== 'undefined') {
+            ReactGA.pageview(window.location.pathname + window.location.search);
+        }
+
+    }, []);
+
+
+
+    return (
+        <>
+            <Head>
+                <title>How To Become A Shopify Developer? Ultimate Roadmap!</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <MainLayout>
+                <div id="blog-page" className="blog-page">
+                <section className="blog-page__highlight-title">
+                                    <h1>How To Become A Shopify Developer? Ultimate Roadmap!</h1>
+                                </section>
+                <section className="blog-content">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-8">
+                                
+
+                                <p>Do you want to become a Shopify developer? If so, you're in the right place! In this guide, we will walk you through everything you need to know in order to make this dream a reality. We'll cover topics such as what Shopify is, what skills you need to be a successful developer, how to find work as a Shopify developer, and more! So whether you're just getting started or are already familiar with some of the basics, this guide has something for everyone. Let's get started!</p>
+
+                                <h2 id="what-is-shopify">What is Shopify?</h2>
+
+                                <p>Shopify is a platform for building online stores. It allows businesses to create, manage, and grow their ecommerce presence with ease. In addition to being user-friendly, Shopify also comes packed with features that can help you build an effective online store. These features include everything from product management and checkout processes to shipping and taxation rules. Plus, because Shopify is so widely used, there's a good chance that any customizations or integrations you need for your business will be available as third-party apps on the Shopify App Store.</p>
+
+                                <h2 id="what-basic-skills-do-i-need-to-become-a-shopify-developer">What basic skills do I need to become a Shopify developer?</h2>
+
+                                <p>If you're looking to become a Shopify developer, there are certain skills you'll need to master. Firstly, you'll need to be comfortable with coding and development concepts. Shopify is built on the Ruby on Rails framework, so a basic understanding of Ruby will be helpful. Additionally, you should have experience working with HTML, CSS, and JavaScript. Finally, it's also beneficial to have some knowledge of ecommerce and online store management concepts. In the next section we will break down all the skills needed for each career path in the shopify ecosystem.</p>
+
+
+
+                                <h2 id="shopify-theme-developer-skills">Shopify Theme Developer Skills</h2>
+
+
+
+                                <ul><li>HTML &amp; CSS</li><li>Javascript</li><li>Jquery(Legacy)</li><li>JS Framework (React or Vue)</li><li>Shopify Platform</li><li>Shopify APIs</li><li>Liquid</li><li>GraphQL</li></ul>
+
+
+
+                                <h2 id="shopify-plus-developer-skills">Shopify Plus Developer Skills</h2>
+
+
+
+                                <ul><li>Shopify Platform</li><li>Liquid</li><li>Shopify Plus Apps</li><li>Script Editor (Ruby)</li><li>Shopify Headless CMS (Strorefront API)</li><li>Framework(Hydrogen or Next JS)</li><li>GraphQL</li></ul>
+
+
+
+                                <h2 id="shopify-app-developer-skills">Shopify App Developer Skills</h2>
+
+
+
+                                <ul>
+                                    <li>Back end Language (Node, PHP, Python, ect...)</li>
+                                    <li>Full stack Development</li>
+                                    <li>Embedded App</li>
+                                    <li>React</li>
+                                    <li>Polaris (Shopify Components)</li>
+                                    <li>App Block</li>
+                                    <li>App Billing</li>
+                                    <li>Shopify POS</li>
+                                    <li>Webhooks</li>
+                                    <li>Marketing (To be successful you need to know how to market your apps)</li>
+                                </ul>
+
+
+
+                                <p></p>
+
+
+
+                                <h2 id="how-can-i-find-work-as-a-shopify-developer">How can I find work as a Shopify developer?</h2>
+
+
+
+                                <p>There are a few different ways to find work as a Shopify developer. One option is to search for jobs on popular job boards such as Indeed or Monster. Another option is to reach out to agencies that specialize in building ecommerce stores using Shopify. These agencies often need developers with specific skillsets, so it's a good idea to make sure your skillset matches what they're looking for. Finally, you can also create a profile on Freelancer or Upwork and start bidding on freelance projects.</p>
+
+
+                                <ul>
+                                <li><a href="https://www.indeed.com/jobs?q=title%3A%20shopify%20developer&amp;l&amp;vjk=1766b8dbe5bed3b4">Indeed</a></li>
+
+
+
+                                <li><a href="https://www.fiverr.com/search/gigs?query=shopify&amp;source=top-bar&amp;search_in=everywhere&amp;search-autocomplete-original-term=shopify">Fiverr</a></li>
+
+
+
+                                <li><a href="https://www.upwork.com/ab/jobs/search/?q=shopify&amp;sort=recency&amp;user_location_match=1">Upwork</a></li>
+
+
+
+                                <li><a href="https://www.shopify.com/careers/search?teams%5B%5D=engineering&amp;keywords=&amp;sort=team_asc">Shopify Careers</a></li>
+
+
+                                </ul>
+                                <h2 id="resources-for-learning-shopify-development">Resources For Learning Shopify Development</h2>
+
+
+                                <ul>
+                                <li><a href="https://codingphase.com/shopify-theme-developer/">Shopify Theme Developer Course</a></li>
+
+
+
+                                <li><a href="https://codingphase.com/shopify-app-developer/">Shopify App Developer Course</a></li>
+
+
+
+                                <li><a href="https://www.shopify.co.uk/partners/shopify-cheat-sheet" target="_blank" rel="noreferrer noopener">Liquid Cheat Sheet</a></li>
+
+
+
+                                <li><a href="https://shopify.github.io/liquid-code-examples/">Liquid Examples</a></li>
+
+
+
+                                <li><a href="https://shopify.dev/themes">Shopify Theme Documentation</a></li>
+
+
+
+                                <li><a href="https://codingphase.com/shopify-app-developer/">Shopify App Development</a></li>
+
+
+
+                                <li><a href="https://www.shopify.com/partners">Shopify Partners</a></li>
+
+
+
+                                <li><a href="https://partner-training.shopify.com/catalog">Shopify Partner Certifications</a></li>
+
+
+
+                                <li><a href="https://www.youtube.com/channel/UCcYsEEKJtpxoO9T-keJZrEw">Shopify Devs Youtube Channel</a></li>
+
+
+
+                                <li><a href="https://www.twitch.tv/shopifydevs">Shopify Devs Twitch Channel</a></li>
+
+
+                                </ul>
+                                <h2 id="what-are-the-benefits-of-becoming-a-shopify-developer">What are the benefits of becoming a Shopify developer?</h2>
+
+
+
+                                <p>There are many benefits to becoming a Shopify developer. Firstly, Shopify is one of the most popular ecommerce platforms in the world, so there's plenty of work available. Secondly, Shopify development is relatively easy to learn compared to other coding languages, making it an ideal choice for beginners. And finally, as a Shopify developer you'll have access to exclusive resources and support from the Shopify team. This includes things like helpful tutorials and documentation, as well as an active community of developers who are always happy to help.</p>
+
+
+
+                                <p>We hope this guide has been helpful in outlining what it takes to become a Shopify developer. If you're interested in learning more, be sure to check out our courses on <a href="https://www.codingphase.com">codingphase.com</a> you can get started with just $20 and get access to over 60+ courses</p>
+
+                            </div>
+
+                        </div>
+
+
+                    </div>
+                </section>
+                </div>
+                <script
+                    async
+                    data-uid="2f89e66125"
+                    src="https://codingphase.ck.page/2f89e66125/index.js"></script>
+            </MainLayout>
+        </>
+    );
+}
